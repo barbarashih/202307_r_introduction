301ce046155efe6a9fba621f68eea44cb87b9110
barbarashih <barbara.shih@roslin.ed.ac.uk>
2023-07-20 08:36:49 +0100

    Add files via upload

--- scripts/TCGAbiolinks.r
+++ scripts/TCGAbiolinks.r
@@ -1,3 +1,8 @@
+#### Script for calculating the significance of gene expression on survival in RNAseq data from TCGA
+# Other notes
+# Include Option to colour samples using markers published by other papers 
+# (see clinical_annotation for the columns starting with paper_) ? (this is for PCA)
+
 library(TCGAbiolinks)
 library(SummarizedExperiment)
 
@@ -31,26 +36,26 @@ normcounts<-cpm(dgelist, normalized.lib.sizes = T)
 
 ### Note
 # Remove control samples (or note down control samples)
-# Option to split by other markers? (more for PCA)
-
-
 tmp_reorganised_log2 <- log2(normcounts + 1)
 
 data_clinic_filt <- data_clinic[, c("bcr_patient_barcode", "vital_status", "days_to_death", "days_to_last_follow_up")]
 data_clinic_filt <- data_clinic_filt[data_clinic_filt$bcr_patient_barcode %in% substr(colnames(tmp_reorganised),1,12),]
 tmp_reorganised_log2 <- data.matrix(tmp_reorganised_log2[,substr(colnames(tmp_reorganised_log2),1,12) %in% data_clinic_filt$bcr_patient_barcode])
 
+# Finding control samples
 group1 <- TCGAquery_SampleTypes(colnames(tmp_reorganised_log2), typesample = c("NT"))
 group2 <- TCGAquery_SampleTypes(colnames(tmp_reorganised_log2), typesample = c("TP"))
 
-tokenStop <- 1
 
+
+#### Calculate the survival for all genes
 tabSurvKMcomplete <- NULL
-
+tokenStop <- 1
 for( i in 1: round(nrow(tmp_reorganised_log2)/100)){
     message( paste( i, "of ", round(nrow(tmp_reorganised_log2)/100)))
+    # run the analysis on batches of 100 genes
     tokenStart <- tokenStop
-    tokenStop <- 100 * i
+	tokenStop <- 100 * i
     tabSurvKM <- TCGAanalyze_SurvivalKM(
         data_clinic_filt,
         tmp_reorganised_log2,
@@ -67,6 +72,7 @@ for( i in 1: round(nrow(tmp_reorganised_log2)/100)){
 tabSurvKMcomplete <- tabSurvKMcomplete[tabSurvKMcomplete$pvalue < 0.01,]
 tabSurvKMcomplete <- tabSurvKMcomplete[order(tabSurvKMcomplete$pvalue, decreasing=F),]
 
+#
 tabSurvKMcompleteDEGs <- tabSurvKMcomplete[
     rownames(tabSurvKMcomplete) %in% dataDEGsFiltLevel$mRNA,
 ]
@@ -74,11 +80,9 @@ tabSurvKMcompleteDEGs <- tabSurvKMcomplete[
 
 
 
-
-
-
-
-
+# Example of how to analyse survival using clinical annotation/annotation 
+# i.e. you can add the annotation for low, medium, high for gene expression onto data_clinic, 
+# then perform survial analysis
 test <- TCGAanalyze_survival(
 data = data_clinic,
 clusterCol = "gender",
@@ -88,51 +92,3 @@ width=10
 )
 
 
-
-
-GDCdownload(query = query_RNASeq, method = "api")
-
-
-####
-## Load the required library
-library('TCGAbiolinks')
-project_name <- "TCGA-ACC"
-
-## Defines the query to the GDC
-query <- GDCquery(project = project_name,
-                  data.category = "Transcriptome Profiling",
-                  data.type = "Gene Expression Quantification",
-                  experimental.strategy = "RNA-Seq",
-                  workflow.type = "STAR - Counts")
-
-## Get metadata matrix
-metadata <- query[[1]][[1]]
-
-## Download data using api
-GDCdownload(query, method = "api")
-
-## Get main directory where data is stored
-main_dir <- file.path("GDCdata", project_name)
-## Get file list of downloaded files
-file_list <- file.path("GDCdata", project_name,list.files(main_dir,recursive = TRUE)) 
-
-## Read first downloaded to get gene names
-test_tab <- read.table(file = file_list[1], sep = '\t', header = TRUE)
-## Delete header lines that don't contain usefull information
-test_tab <- test_tab[-c(1:4),]
-## STAR counts and tpm datasets
-tpm_data_frame <- data.frame(test_tab[,1])
-count_data_frame <- data.frame(test_tab[,1])
-
-## Append cycle to get the complete matrix
-for (i in c(1:length(file_list))) {
-  ## Read table
-  test_tab <- read.table(file = file_list[i], sep = '\t', header = TRUE)
-  ## Delete unwanted lines
-  test_tab <- test_tab[-c(1:4),]
-  ## Column bind of tpm and counts data
-  tpm_data_frame <- cbind(tpm_data_frame, test_tab[,7])
-  count_data_frame <- cbind(count_data_frame, test_tab[,4])
-  ## Print progres from 0 to 1
-  print(i/length(file_list))
-}
