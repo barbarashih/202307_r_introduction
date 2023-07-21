e372f4ffd86c44448169cfd60418fc7c371b8713
barbarashih <barbara.shih@roslin.ed.ac.uk>
2023-07-21 15:15:24 +0100

    Add files via upload

--- scripts/TCGAbiolinks.r
+++ scripts/TCGAbiolinks.r
@@ -9,7 +9,7 @@ library(TCGAbiolinks)
 library(SummarizedExperiment)
 library(edgeR)
 
-setwd("C:/Users/bshih/Documents/tmp/nwcr")
+setwd("\\\\lancs/homes/43/shihb/My Documents/r_working")
 
 #### Define query
 project <- "TCGA-SKCM"
@@ -22,6 +22,7 @@ query_RNASeq <- GDCquery(project = project,
 RNAseq_samples <- getResults(query_RNASeq)
 
 #### Prepare data from query
+GDCdownload(query_RNASeq)
 data_RNAseq <- GDCprepare(query_RNASeq)
 data_clinic <- GDCquery_clinic(project, "clinical")
 
@@ -29,7 +30,7 @@ data_clinic <- GDCquery_clinic(project, "clinical")
 # https://bioconductor.org/packages/devel/bioc/vignettes/TCGAbiolinks/inst/doc/download_prepare.html#GDCdownload
 # tpm_mx <- assay(data_RNAseq, "tpm_unstrand") # tpm matrix; don't need this
 count_mx <- assay(data_RNAseq, "unstranded") # gene count matrix
-clinical_annotation <- colData(data_RNAseq) # clinical annotation data
+sample_annotation <- colData(data_RNAseq) # clinical annotation data
 gene_annotation <- rowData(data_RNAseq) # gene annotation data
 
 # Normalise count matrix 
@@ -46,12 +47,13 @@ normcounts<-cpm(dgelist, normalized.lib.sizes = T)
 tmp_reorganised_log2 <- log2(normcounts + 1)
 
 data_clinic_filt <- data_clinic[, c("bcr_patient_barcode", "vital_status", "days_to_death", "days_to_last_follow_up")]
-data_clinic_filt <- data_clinic_filt[data_clinic_filt$bcr_patient_barcode %in% substr(colnames(tmp_reorganised),1,12),]
+data_clinic_filt <- data_clinic_filt[data_clinic_filt$bcr_patient_barcode %in% substr(colnames(tmp_reorganised_log2),1,12),]
 tmp_reorganised_log2 <- data.matrix(tmp_reorganised_log2[,substr(colnames(tmp_reorganised_log2),1,12) %in% data_clinic_filt$bcr_patient_barcode])
 
 # Finding control samples
-group1 <- TCGAquery_SampleTypes(colnames(tmp_reorganised_log2), typesample = c("NT"))
-group2 <- TCGAquery_SampleTypes(colnames(tmp_reorganised_log2), typesample = c("TP"))
+group1 <- TCGAquery_SampleTypes(colnames(tmp_reorganised_log2), typesample = c("NT", "NB", "NBC", "NEBV", "NBM"))
+group2 <- TCGAquery_SampleTypes(colnames(tmp_reorganised_log2), typesample = c("TP", "TR", "TB", "TAP"))
+group3 <- TCGAquery_SampleTypes(colnames(tmp_reorganised_log2), typesample = c("TRBM", "TM", "TB", "TAP")) 
 
 
 
@@ -94,10 +96,44 @@ tabSurvKMcompleteDEGs <- tabSurvKMcomplete[
 # then perform survial analysis
 test <- TCGAanalyze_survival(
 data = data_clinic,
-clusterCol = "gender",
+clusterCol = "ajcc_pathologic_stage",
 main = "TCGA Set\n GBM",
 height = 10,
 width=10
 )
 
 
+library(ggplot2)
+library(gridExtra)
+library(ggpubr)
+library(grid)
+
+for(gene in c("GLIS1", "GLIS2", "GLIS3")){
+#for(gene in "RNF169"){
+	gene_ensembl <- gene_annotation$gene_id[gene_annotation$gene_name == gene]
+
+	plot_df <- data.frame(gene= as.numeric(as.data.frame(normcounts)[gene_ensembl,]), 
+		sample_type = sample_annotation$sample_type, 
+		ajcc_pathologic_stage = sample_annotation$ajcc_pathologic_stage,
+		ajcc_pathologic_t = sample_annotation$ajcc_pathologic_t,
+		ajcc_pathologic_m = sample_annotation$ajcc_pathologic_m,
+		ajcc_pathologic_n = sample_annotation$ajcc_pathologic_n
+		)
+	if(TRUE){
+		p1 <- ggplot(data = plot_df, aes(y=gene, x=sample_type)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw() + scale_y_log10()
+		p2 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_stage)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()  + scale_y_log10() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+		p3 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_t)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()  + scale_y_log10()
+		p4 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_m)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()  + scale_y_log10()
+		p5 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_n)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()  + scale_y_log10()
+	} else {
+		p1 <- ggplot(data = plot_df, aes(y=gene, x=sample_type)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()
+		p2 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_stage)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+		p3 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_t)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()  
+		p4 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_m)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw() 
+		p5 <- ggplot(data = plot_df, aes(y=gene, x=ajcc_pathologic_n)) + geom_boxplot(outlier.shape=NA) + geom_point(position=position_jitter(w=0.2)) + theme_bw()  
+	}
+	g <- arrangeGrob(p1, p2, p3, p4,p5,
+				 ncol = 2, nrow = 3, top=text_grob(gene))
+	ggsave(paste0(gene, ".png"), g)
+	
+}
